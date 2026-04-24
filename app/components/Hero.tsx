c11ee9bba22687567d514caef2f19861e79ff7d2
Stefan1400 <stefan.besonen783@gmail.com>
2026-04-24 12:05:34 -0700

    fix(hero): fix insufficient text contrast on iOS Safari

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,12 +4,12 @@ export default function Hero() {
 
         <div>
 
-          <h1 className="font-semibold text-4xl md:text-6xl lg:text-7xl">
+          <h1 className="font-semibold text-4xl md:text-6xl lg:text-7xl text-white">
             STEFAN BESONEN
           </h1>
 
           <div className="mt-10 flex flex-col gap-3 text-sm md:text-base lg:text-lg">
-            <p className="font-medium">Full stack developer</p>
+            <p className="font-medium text-white">Full stack developer</p>
             <p className="text-[#C6C6C6]">
               I build clean, minimal, user-focused web applications.
             </p>
