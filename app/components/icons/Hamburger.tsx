75c429374bab3629c48ff93a61090388ca308a71
Stefan1400 <stefan.besonen783@gmail.com>
2026-04-25 12:45:03 -0700

    refactor(nav): improve semantics and component structure - switched desktop logo from div to anchor - added type="button" to hamburger button - fixed mobile responsive classes - replaced div navigation with semantic ul/li structure - moved SVGs into reusable components

---
+++ app/components/icons/Hamburger.tsx
@@ -0,0 +1,8 @@
+export default function Hamburger() {
+  return (
+    <svg width="30" height="15" viewBox="0 0 30 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="30" height="1" rx="0.5" fill="white"/>
+      <rect y="14" width="30" height="1" rx="0.5" fill="white"/>
+   </svg>
+  )
+}
