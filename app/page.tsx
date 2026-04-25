e47d07ab8f4bf88c533ddd841a79bc1ab3751529
Stefan1400 <stefan.besonen783@gmail.com>
2026-04-25 12:18:20 -0700

    feat(nav): add navbar UI with section links

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Navbar />
       <Hero />
     </div>
   );
